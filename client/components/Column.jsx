f7f1017a2b2011f42e7dfe386ba138e28c08b31e
ryanmcd <rp.mcdan@gmail.com>
2021-03-25 17:13:23 -0400

    successfully prop drilled down to the bottom schema level and rendered in browser

--- client/components/Column.jsx
+++ client/components/Column.jsx
@@ -1,36 +1,42 @@
 import React from 'react';
+import ColumnsDisplay from './ColumnsDisplay';
 
 export default function Column (props) {
   console.log('props.columnName', props.columnName);
   console.log('props.columnValue', props.columnValue);
 
-  // const currSchema = [];
+  const currentSchema = [];
 
-  // for (let i = 0; i < Object.keys(props.tableContent.columns).length; i++) {
-  //   // const key = Object.keys(props.tableContent)[i];
-  //   // const value = props.tableContent[i];
+  for (let i = 0; i < Object.keys(props.columnValue).length; i++) {
+    // const key = Object.keys(props.tableContent)[i];
+    // const value = props.tableContent[i];
 
-  //   const key = Object.keys(props.tableContent.columns)[i];
-  //   const value = Object.values(props.tableContent.columns)[i];
+    // const key = Object.keys(props.currentSchema)[i];
 
-  //   // console.log('key: ', key)
-  //   // console.log('value: ', value)
+    const key = Object.keys(props.columnValue)[i];
+    const value = Object.values(props.columnValue)[i];
+    
+    console.log('KEY: ', key);
+    console.log('VALUE: ', value);
 
-  //   tableColumns.push(
-  //     <Column 
-  //       key={`Column${i}`}
-  //       columnName={key}
-  //       columnValue={value}
-  //     />
-  //   )
-  // }
+    currentSchema.push(
+      <ColumnsDisplay 
+        key={`ColumnsDisplay${i}`}
+        columnName={props.columnName}
+        propertyKey={key}
+        propertyValue={value}
+      />
+    )
+  }
+
+    console.log('currentSchema: ', currentSchema);
 
   return (
     <div>
-      <h6>
+      <h6 style={{color:'red'}}>
         {props.columnName}
       </h6>
-      {/* {tableColumns} */}
+      {currentSchema}
     </div>
   )
 }
--- client/components/ColumnsDisplay.jsx
+++ client/components/ColumnsDisplay.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
 
 export default function ColumnsDisplay (props) {
-
-  const tableColumns = [];
-
-  for (let i = 0; i < Object.keys(props.tableContent.columns).length; i++) {
-    const key = Object.keys(props.tableContent.columns)[i];
-    // const value = Object.values(props.tableContent.columns)[i];
-
-    console.log('key: ', key)
-    // console.log('value: ', value)
-
-    tableColumns.push(
-      <Column 
-        key={`Column${i}`}
-        columnName={key}
-        // columnValue={value}
-      />
-    )
-  }
-  
-  console.log('tableColumns: ', tableColumns);
   
   return (
     <div>
-      <h1>
-      {tableColumns}
-      {/* {props.tableContent} */}
-      </h1>
+      <p>
+        {props.propertyKey}
+        <br></br>
+        {props.propertyValue}
+      </p>
     </div>
   )
 }
