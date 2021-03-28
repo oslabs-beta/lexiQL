5fdcad97d9e6c17c5c62d6ab2438bc6ed470464f
Carthanial <ccarney51@gmail.com>
2021-03-28 15:29:19 -0400

    Updated createGQLSchema controller
    	Created initial logic and the error handler.
    	Created testing route for example-schema called example-schema-json

--- server/GQLFactory/schemaFactory.js
+++ server/GQLFactory/schemaFactory.js
@@ -1,12 +1,14 @@
-const { queries, mutations, customObjects } = require('./typeFactory');
-const { collectQueries, collectMutations, collectCustomObjectRelationships } = require('./resolverFactory');
-const isJoinTable = require('./helperfunctions');
+const { queries, mutations, customObjects } = require("./typeFactory");
+const {
+  collectQueries,
+  collectMutations,
+  collectCustomObjectRelationships,
+} = require("./resolverFactory");
+const isJoinTable = require("./helperfunctions");
 /*    High level functions tasked with assembling the Types and the Resolvers */
 schemaFactory = {};
 /*  Creates query, mutation, and custom Object Types  */
-schemaFactory.createTypes = (tableName, tableData) => {
-
-};
+schemaFactory.createTypes = (sqlSchema) => {};
 
 schemaFactory.createResolvers = () => {};
 
--- server/controllers/SQLController.js
+++ server/controllers/SQLController.js
@@ -1,35 +1,32 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 /* Example db URI */
-const EX_PG_URI = 'postgres://zhocexop:Ipv9EKas6bU6z9ehDXZQRorjITIXijGv@ziggy.db.elephantsql.com:5432/zhocexop';
-const fs = require('fs');
-const sqlQuery = fs.readFileSync('server/tableQuery.sql', 'utf8');
-
+const EX_PG_URI =
+  "postgres://zhocexop:Ipv9EKas6bU6z9ehDXZQRorjITIXijGv@ziggy.db.elephantsql.com:5432/zhocexop";
+const fs = require("fs");
+const sqlQuery = fs.readFileSync("server/tableQuery.sql", "utf8");
 
 const SQLController = {};
 
 SQLController.getSQLSchema = (req, res, next) => {
-    let PSQL_URI;
-    console.log('request body', req.body);
-    req.body.link ? PSQL_URI = req.body.link : PSQL_URI = EX_PG_URI
-    const db = new Pool({connectionString: PSQL_URI});
-    db.query(sqlQuery)
+  let PSQL_URI;
+  req.body.link ? (PSQL_URI = req.body.link) : (PSQL_URI = EX_PG_URI);
+  const db = new Pool({ connectionString: PSQL_URI });
+  db.query(sqlQuery)
     .then((data) => {
-        res.locals.SQLSchema = data.rows[0].tables;
-        return next()
+      res.locals.SQLSchema = data.rows[0].tables;
+      return next();
     })
     .catch((err) => {
-        const errObj = {
-            log: `Error in getSQLSchema: ${err}`,
-            status: 400,
-            message: {
-              err: 'Unable to connect to SQL database, please confirm URI',
-            },
-          };
-          return next(errObj);
-        });
+      const errObj = {
+        log: `Error in getSQLSchema: ${err}`,
+        status: 400,
+        message: {
+          err: "Unable to connect to SQL database, please confirm URI",
+        },
+      };
+      return next(errObj);
+    });
 };
 /* Format the SQL Schema for visualizer */
-SQLController.createGraphData = (req, res, next) => {
-  
-}
-module.exports = SQLController
+SQLController.createGraphData = (req, res, next) => {};
+module.exports = SQLController;
