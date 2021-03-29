a144b93c7a0f3dd79bc23e6424eb2c4eb94d8d0b
ryanmcd <rp.mcdan@gmail.com>
2021-03-29 19:35:02 -0400

    major styling changes to homepage

--- client/containers/introContainer.jsx
+++ client/containers/introContainer.jsx
@@ -1,29 +1,30 @@
 import React from 'react';
+import Logo from '../assets/lexiql-logo.png';
 
 export default function introContainer() {
   return (
     <div id="about">
-      <div className="aboutProject">
-        <h3>Header #1</h3>
+      <img id='logo' src={Logo} alt='logo' />
+      {/* <div className="aboutProject">
+        <h3>What do we do?</h3>
         <p>
-          Here is a description about lexiQL and why you should totally use our
-          tool.
+          Provide you with a <i>lexical</i> framework for your GraphQL schemas and resolvers.
         </p>
-      </div>
-      <div className="aboutProject">
+      </div> */}
+      {/* <div className="aboutProject">
         <h3>Header #2</h3>
         <p>
           This writing is about our second awesome header and all the reasons
           you should use our tool!
         </p>
-      </div>
-      <div className="aboutProject">
+      </div> */}
+      {/* <div className="aboutProject">
         <h3>Featured on Medium</h3>
         <p>
           Here's a third absolutely killer reason you should use this kickin'
           tool. Seriously, just use it.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
