c8ad083dba20d9cfe3ab372c091247a4591c9c3c
johnli <jli159@binghamton.edu>
2021-03-30 21:59:43 -0400

    Updated resolverFactory.js and resolverHelpers.js for resolver mutations

--- server/GQLFactory/helpers/resolverHelpers.js
+++ server/GQLFactory/helpers/resolverHelpers.js
@@ -1,6 +1,6 @@
-const toCamelCase = require("camelcase");
-const { singular } = require("pluralize");
-const { pascalCase } = require("pascal-case");
+const toCamelCase = require('camelcase');
+const { singular } = require('pluralize');
+const { pascalCase } = require('pascal-case');
 
 const resolverHelper = {};
 
@@ -12,11 +12,63 @@ resolverHelper.queryAll = () => {};
 
 /* */
 
-resolverHelper.createMutation = () => {};
+resolverHelper.createMutation = (tableName, primaryKey, columns) => {
+  const mutationName = toCamelCase('add_' + singular(tableName));
+  const columnsArray = Object.keys(columns).filter(
+    (column) => column !== primaryKey
+  );
+  const columnsArgument = columnsArray.join(', ');
+  const valuesArgument = columnsArray
+    .map((column, i) => `$${i + 1}`)
+    .join(', ');
+  const valuesList = columnsArray.map((column) => `args.${column}`).join(', ');
 
-resolverHelper.updateMutation = () => {};
+  return `
+			${mutationName}: (parent, args) => {
+				const query = 'INSERT INTO ${tableName} (${columnsArgument}) VALUES (${valuesArgument}) RETURNING *';
+				const values = [${valuesList}];
+				return db.query(query, values)
+					.then(data => data.rows[0])
+					.catch(err => new Error(err));
+			},`;
+};
 
-resolverHelper.deleteMutation = () => {};
+resolverHelper.updateMutation = (tableName, primaryKey, columns) => {
+  const mutationName = toCamelCase('update_' + singular(tableName));
+  const columnsArray = Object.keys(columns).filter(
+    (column) => column != primaryKey
+  );
+  const setStatement = columnsArray
+    .map((column, i) => `${column} = $${i + 1}`)
+    .join(', ');
+  const valuesList = [
+    columnsArray.map((column) => `args.${column}`).join(', ') +
+      `, args.${primaryKey}`,
+  ];
+  const primaryKeyArgument = `$${columnsArray.length + 1}`;
+
+  return `
+			${mutationName}: (parent, args) => {
+				const query = 'UPDATE ${tableName} SET ${setStatement} WHERE ${primaryKey} = ${primaryKeyArgument} RETURNING *';
+				const values = ${valuesList};
+				return db.query(query, values)
+					.then(data => data.rows[0])
+					.catch(err => new Error(err));
+			},`;
+};
+
+resolverHelper.deleteMutation = (tableName, primaryKey) => {
+  const mutationName = toCamelCase('delete_' + singular(tableName));
+
+  return `
+			${mutationName}: (parent, args) => {
+				const query = 'DELETE FROM ${tableName} WHERE ${primaryKey} = $1 RETURNING *';
+				const values = [args.${primaryKey}];
+				return db.query(query, values)
+					.then(data => data.rows[0])
+					.catch(err => new Error(err));
+			},`;
+};
 
 /* */
 
--- server/GQLFactory/resolverFactory.js
+++ server/GQLFactory/resolverFactory.js
@@ -5,7 +5,21 @@ const resolverFactory = {};
 
 resolverFactory.collectQueries = () => {};
 /* -------------------------------- */
-resolverFactory.collectMutations = () => {};
+resolverFactory.collectMutations = (tableName, tableData) => {
+  const { primaryKey, columns } = tableData;
+  const createMutation = resolverHelper.createMutation(
+    tableName,
+    primaryKey,
+    columns
+  );
+  const updateMutation = resolverHelper.updateMutation(
+    tableName,
+    primaryKey,
+    columns
+  );
+  const deleteMutation = resolverHelper.deleteMutation(tableName, primaryKey);
+  return `${createMutation}\n${updateMutation}\n${deleteMutation}`;
+};
 /* ------------------------------------ */
 resolverFactory.collectCustomObjectRelationships = () => {};
 
