e88922093f98c7311cf56a760a8f8d5b13f38e89
ryanmcd <rp.mcdan@gmail.com>
2021-04-01 10:53:15 -0400

    last commit before merge

--- client/components/nodeElements.jsx
+++ client/components/nodeElements.jsx
@@ -1,68 +1,110 @@
-import React, { useState, Fragment, useContext } from 'react';
-import ReactFlow, {
-  addEdge,
-  Background,
-  Controls,
-  MiniMap,
-} from 'react-flow-renderer';
-import { VisualizerContext } from '../state/contexts';
+import React from 'react';
 
-// const initialElements = [
-//   { id: '1', type: 'input', data: { label: 'Node' }, position: { x: 0, y: 0 } },
-// ];
-
-const onLoad = (reactFlowInstance) => {
-  reactFlowInstance.fitView();
-};
-
-export default function tableNode() {
-  // console.log('inside table node', sqlSchema);
-  const { visualizerState } = useContext(VisualizerContext);
-  // const [elements, setElements] = useState(initialElements);
-  // const [name, setName] = useState('');
-
-  // const addNode = () => {
-  //   setElements((e) =>
-  //     e.concat({
-  //       id: (e.length + 1).toString(),
-  //       data: { label: `${name}` },
-  //       position: {
-  //         x: Math.random() * window.innerWidth,
-  //         y: Math.random() * window.innerHeight,
-  //       },
-  //     }),
-  //   );
-  // };
-
-  // this lets you connect to other nodes
-
-  const onConnect = (params) => setElements((e) => addEdge(params, e));
-
-  return (
-    <Fragment>
-      <ReactFlow
-        minzoom={0.6}
-        maxzoom={1}
-        defaultzoom={.9}
-        // zoomOnScroll={zoomOnScroll}
-        elements={visualizerState.tableNodes}
-        onLoad={onLoad}
-        style={{ width: '100%', height: '90vh' }}
-        onConnect={onConnect}
-        connectionLineStyle={{ stroke: '#ddd', strokeWidth: 2 }}
-        connectionLineType="bezier"
-        snapToGrid={true}
-        snapGrid={[16, 16]}
-      >
-        <Background color="#888" gap={16} />
-        {/* 
-        <MiniMap
-          nodeColor={(n) => {
-            if (n.type === 'input') return 'blue';
-            return '#FFCC00';
-          }}
-        /> */}
-      </ReactFlow>
-    </Fragment>
-  );
-}
+export funct
+export const columnElements = [
+  {
+    id: '1',
+    type: 'input',
+    data: {
+      label: (
+        <>
+          Welcome to <strong>React Flow!</strong>
+        </>
+      ),
+    },
+    position: { x: 250, y: 0 },
+  },
+  {
+    id: '2',
+    data: {
+      label: (
+        <>
+          This is a <strong>default node</strong>
+        </>
+      ),
+    },
+    position: { x: 100, y: 100 },
+  },
+  {
+    id: '3',
+    data: {
+      label: (
+        <>
+          This one has a <strong>custom style</strong>
+        </>
+      ),
+    },
+    position: { x: 400, y: 100 },
+    style: {
+      background: '#D6D5E6',
+      color: '#333',
+      border: '1px solid #222138',
+      width: 180,
+    },
+  },
+  {
+    id: '4',
+    position: { x: 250, y: 200 },
+    data: {
+      label: 'Another default node',
+    },
+  },
+  {
+    id: '5',
+    data: {
+      label: 'Node id: 5',
+    },
+    position: { x: 250, y: 325 },
+  },
+  {
+    id: '6',
+    type: 'output',
+    data: {
+      label: (
+        <>
+          An <strong>output node</strong>
+        </>
+      ),
+    },
+    position: { x: 100, y: 480 },
+  },
+  {
+    id: '7',
+    type: 'output',
+    data: { label: 'Another output node' },
+    position: { x: 400, y: 450 },
+  },
+  { id: 'e1-2', source: '1', target: '2', label: 'this is an edge label' },
+  { id: 'e1-3', source: '1', target: '3' },
+  {
+    id: 'e3-4',
+    source: '3',
+    target: '4',
+    animated: true,
+    label: 'animated edge',
+  },
+  {
+    id: 'e4-5',
+    source: '4',
+    target: '5',
+    arrowHeadType: 'arrowclosed',
+    label: 'edge with arrow head',
+  },
+  {
+    id: 'e5-6',
+    source: '5',
+    target: '6',
+    type: 'smoothstep',
+    label: 'smooth step edge',
+  },
+  {
+    id: 'e5-7',
+    source: '5',
+    target: '7',
+    type: 'step',
+    style: { stroke: '#f6ab6c' },
+    label: 'a step edge',
+    animated: true,
+    labelStyle: { fill: '#f6ab6c', fontWeight: 700 },
+  },
+];
