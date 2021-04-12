bc7f02f0bfbf02b6d19305e5dd2177151b90dd66
ryanmcd <rp.mcdan@gmail.com>
2021-04-11 20:14:23 -0400

    changed color palate, added new navbar links

--- client/components/URIForm.jsx
+++ client/components/URIForm.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react';
-import URIbtn from './URIbtn';
-import { FormContext } from '../state/contexts';
+import React, { useContext } from "react";
+import URIbtn from "./URIbtn";
+import { FormContext } from "../state/contexts";
 
 export default function URIForm() {
   const {
@@ -16,7 +16,7 @@ export default function URIForm() {
   const handleSampleData = (e) => {
     e.preventDefault();
 
-    fetch('/example-schema')
+    fetch("/example-schema")
       .then((res) => res.json())
       .then((data) => {
         const sqlSchema = data.SQLSchema;
@@ -29,8 +29,8 @@ export default function URIForm() {
 
           tableNodes.push({
             id: i.toString(),
-            type: 'default',
-            style: { background: ' #5a95f5' },
+            type: "default",
+            style: { background: "#5a95f5" },
             data: { label: tableName },
 
             position: {
@@ -45,8 +45,8 @@ export default function URIForm() {
             const columnLabel = Object.keys(columns[j])[0];
             tableNodes.push({
               id: `${i}${j}`,
-              type: 'default',
-              style: { background: '#f5ba5a' },
+              type: "default",
+              style: { background: "#f5ba5a" },
               data: { label: columnLabel },
 
               position: {
@@ -58,7 +58,7 @@ export default function URIForm() {
         }
 
         diagramDispatch({
-          type: 'SET_TABLES',
+          type: "SET_TABLES",
           payload: {
             sqlSchema,
             tableNodes,
@@ -66,7 +66,7 @@ export default function URIForm() {
         });
 
         codeDispatch({
-          type: 'SET_CODE',
+          type: "SET_CODE",
           payload: {
             schema: data.GQLSchema.types,
             resolver: data.GQLSchema.resolvers,
@@ -75,7 +75,7 @@ export default function URIForm() {
         });
 
         formDispatch({
-          type: 'TOGGLE_FORM',
+          type: "TOGGLE_FORM",
           payload: {
             firstFetch: false,
             formIsOpen: false,
@@ -87,18 +87,18 @@ export default function URIForm() {
   // get data from user input DB
   const handleURI = (e) => {
     e.preventDefault();
-    const URILink = document.getElementById('URILink').value;
+    const URILink = document.getElementById("URILink").value;
     const valid = /^postgres:\/\//g;
 
     // if there is no input or if input is invalid do nothing
     if (!URILink || !valid.test(URILink))
       return alert(
-        'Missing URI link or the link is invalid. Please enter a valid URI link.',
+        "Missing URI link or the link is invalid. Please enter a valid URI link."
       );
 
-    fetch('/sql-schema', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+    fetch("/sql-schema", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ link: URILink }),
     })
       .then((res) => res.json())
@@ -113,8 +113,8 @@ export default function URIForm() {
 
           tableNodes.push({
             id: i.toString(),
-            type: 'default',
-            style: { background: ' #5a95f5' },
+            type: "default",
+            style: { background: " #5a95f5" },
             data: { label: tableName },
 
             position: {
@@ -129,8 +129,8 @@ export default function URIForm() {
             const columnLabel = Object.keys(columns[j])[0];
             tableNodes.push({
               id: `${i}${j}`,
-              type: 'default',
-              style: { background: '#f5ba5a' },
+              type: "default",
+              style: { background: "#f5ba5a" },
               // style: { background:' #5a95f5' },
               data: { label: columnLabel },
 
@@ -143,7 +143,7 @@ export default function URIForm() {
         }
 
         diagramDispatch({
-          type: 'SET_TABLES',
+          type: "SET_TABLES",
           payload: {
             sqlSchema,
             tableNodes,
@@ -151,7 +151,7 @@ export default function URIForm() {
         });
 
         codeDispatch({
-          type: 'SET_CODE',
+          type: "SET_CODE",
           payload: {
             schema: data.GQLSchema.types,
             resolver: data.GQLSchema.resolvers,
@@ -160,7 +160,7 @@ export default function URIForm() {
         });
 
         formDispatch({
-          type: 'TOGGLE_FORM',
+          type: "TOGGLE_FORM",
           payload: {
             firstFetch: false,
             formIsOpen: false,
@@ -170,9 +170,9 @@ export default function URIForm() {
   };
 
   // don't have URI form toggle button appear if it's the user's first time on the page
-  let btnDisplay = '';
+  let btnDisplay = "";
   if (formState.firstFetch) {
-    btnDisplay = '';
+    btnDisplay = "";
   } else {
     btnDisplay = <URIbtn />;
   }
@@ -180,7 +180,7 @@ export default function URIForm() {
   return (
     <div className="uriForm" id="uriForm">
       {btnDisplay}
-      <div className={formState.formIsOpen ? 'uripanel open' : 'uripanel'}>
+      <div className={formState.formIsOpen ? "uripanel open" : "uripanel"}>
         <form onSubmit={handleURI}>
           <label className="formHeader" htmlFor="link">
             Link a database:
--- client/index.js
+++ client/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-import './style.scss';
-import './codeMirror.scss';
+import "./stylesheets/index.scss";
 
 render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
